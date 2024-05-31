f0f0d5b89bd7e10e0fe2a30fba8b2675dcb4e8bb
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-05-31 11:04:55 +0530

    added text on image

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -2,15 +2,30 @@ import Image from "next/image";
 import Link from "next/link";
  import landscape from '@/public/landscape.jpg'
 import { Navbar } from "../navbar/page";
+import { acme } from '@/app/ui/fonts';
 export function Landing(){
     return(
         <div>
-            {/* <h1>Landing</h1>
-            <Link href="/ui/login">Login</Link> */}
-            <Navbar/>
+          
+                      <Navbar/>
+<div className="flex flex-col items-center justify-between ">
+         
+            <div className="">
             <Image src={landscape}
             alt="landscape"
-            className='w-full h-full object-cover lg:h-auto' priority/>
-        </div>
+            className="priority w-full" 
+            // width={1000}
+            // height={1000}
+            />
+            </div>
+            <div className=" absolute max-w-screen-lg m-auto-h-[200vh] py-5 px-5 my-10 text-center text-6xl text-violet font-serif font-extrabold">
+            
+            <h1>My Diary Tales</h1>
+            </div>
+            </div>
+               </div> 
+              
+           
+        
     )
 }
