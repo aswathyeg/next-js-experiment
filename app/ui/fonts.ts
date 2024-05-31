1e05c580a0d116275f960208417a5ea615affd27
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-05-31 11:06:10 +0530

    added acme font

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Acme } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const acme = Acme({
+    weight: ['400', '400'],
+    subsets: ['latin'],
+  });
