c6ea8f7124e4ab5db70d73018986321df74844f7
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-05-31 11:05:30 +0530

    added link,updated image

--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 import Diary from "@/public/diary-black.svg"
 import User from "@/public/user.svg"
 import Menu from "@/public/menu.svg"
+import Link from "next/link";
 export function Navbar(){
     const nanvLinks=[
-        {name:"Features"},
-        {name:"Pricing"},
-        {name:"Enterprise"},
-        {name:"Careers"}
+        {name:"Home"},
+        {name:"Your Life,stories"},
+        {name:"Feature"},
+        {name:"Subscription"}
     ]
     return(
         <nav className="flex w-full items-center justify-between px-[5px] py-[14px]  
@@ -16,11 +17,12 @@ export function Navbar(){
             <Image
             src={Diary}
             alt="logo"
-            width={70}
-            height={50}
+            width={60}
+            height={40}
+            className="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300 ..."
             />
            
-             <div className="hidden lg:flex pl-[74px] gap-x-[56px] px-[20px] py-[16px]">
+             <div className="hidden lg:flex pl-[74px] gap-x-[56px] px-[20px] py-[16px] text-violet ">
                {nanvLinks.map((item,index)=>
                 (
                     <p className="text-[##36485c] font-medium" key={index}>{item.name}</p>
@@ -29,8 +31,9 @@ export function Navbar(){
             </div>
             
             <div className="flex items-center gap-x-[56px]">
-            <span className="hidden lg:block font-medium text-[#36485c]">Open Account</span>
-            <span className="hidden lg:block font-medium text-[#36485c] ">Sign in</span>
+            <span className="hidden lg:block font-medium text-violet">Open Account</span>
+            <Link href="/ui/login"><span className="hidden lg:block font-medium text-violet ">Sign in</span></Link>
+            
                 <Image
                  src={User}
                 alt="user"
