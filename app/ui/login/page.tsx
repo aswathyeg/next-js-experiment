39e0796a29b3ac1217d13f0c66e5cd8cd8e66a06
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-05-31 11:06:29 +0530

    added sign up

--- app/ui/login/page.tsx
+++ app/ui/login/page.tsx
@@ -7,27 +7,29 @@ export default function LoginPage(){
             <div className="w-full h-screen flex justify-center items-center bg-black opacity-80">
                 <aside className="bg-white w-full max-w-md rounded-xl bg-opacity-20 shadow-lg shadow-black">
             <h1 className="text-center text-black font-dark text-4xl bg-white rounded-t-xl m-0 py-4">
-                Sign In
+                Log In
             </h1>
             <form action=""
-            className="p-6">
+            className="p-6 pb-2">
                 <input type="text"
                 name=""
                 placeholder="Username"
-                className="py-2 px-3 w-full text-black text-lg font-dark outline-darkRed "
+                className="py-2 px-3 w-full text-black text-lg font-dark outline-darkRed rounded hover:rounded-[16px]"
                 />
                 <input type="password"
                 name=""
                 placeholder="Password"
-                className="py-2 px-3 w-full text-black text-lg font-dark outline-darkRed mt-3"
+                className="py-2 px-3 w-full text-black text-lg font-dark outline-darkRed mt-3
+                rounded hover:rounded-[16px]"
                 />
                 <div className="flex mt-5 justify-between items-center  font-bold py-2 px-8">
-                    <Link href=""
+                    <Link href="/ui/sign-up"
                     className="text-white cursor-pointer 
                     transition hover:text-darkRed"
                     >Not Yet Registered ?</Link>
                     <button type="submit"
-                   className="bg-black text-white  py-2 px-8 transition hover:text-darkRed" >
+                   className="bg-black text-white  py-2 px-8 transition hover:text-darkRed
+                   rounded hover:rounded-[16px]" >
                         Sign In
                     </button>
                 </div>
