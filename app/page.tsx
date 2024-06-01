5a794edae269e4058798eab3dce72830649d3e5e
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-06-01 18:57:45 +0530

    updated classname

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import { Landing } from "./landing/page";
+
+
 export default function Home() {
   return (
-     <main className="flex min-h-screen flex-col items-center justify-between">
+     <main className="flex min-h-screen flex-col bg-pageBg bg-cover bg-center">
       <div>
         <Landing/>
+    
       </div>
      </main>
   );
