b64a20105fdc6ba4a6113f21520a977c17b52823
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-06-04 21:46:15 +0530

    updated font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,10 @@
-import { Inter, Acme } from 'next/font/google';
+import { Inter, Prompt } from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
-export const acme = Acme({
-    weight: ['400', '400'],
+export const prompt = Prompt({
+    weight: ['700','700'],
     subsets: ['latin'],
   });
+  // export const tenorSans = Tenor_Sans({
+  //   weight: ['400','400'],
+  //   subsets: ['latin'],
+  // });
