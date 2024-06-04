1ad6dba48b3a334b2c1eb943a5528b88cc05bf41
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-06-04 12:57:11 +0530

    updated navbar

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,30 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
- import landscape from '@/public/landscape.jpg'
+import landscape from "@/public/landscape.jpg";
 import { Navbar } from "../navbar/page";
-import { acme } from '@/app/ui/fonts';
-export function Landing(){
-    return(
-        <div>
-          
-                      <Navbar/>
-<div className="flex flex-col items-center justify-between">
-         
-            
-            {/* <Image src={landscape}
+import { acme } from "@/app/ui/fonts";
+export function Landing() {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-between">
+        {/* <Image src={landscape}
             alt="landscape"
             className="priority w-full" 
             
             /> */}
-            
-            <div className=" absolute max-w-screen-lg m-auto-h-[200vh] py-5 px-5 my-10 text-center text-6xl text-violet font-serif font-extrabold">
-            
-            <h1>My Diary Tales</h1>
-            </div>
-            </div>
-               </div> 
-              
-           
-        
-    )
+
+        <div className=" absolute max-w-screen-lg m-auto-h-[200vh] py-5 px-5 my-10 text-center text-6xl text-violet font-serif font-extrabold">
+          <h1>My Diary Tales</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Landing } from "./landing/page";
-
+import { Navbar } from "./navbar/page";
+Navbar
 
 export default function Home() {
   return (
      <main className="flex min-h-screen flex-col bg-pageBg bg-cover bg-center">
       <div>
+      <Navbar/>
         <Landing/>
     
       </div>
