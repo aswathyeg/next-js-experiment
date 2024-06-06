8f691160e31d9c534c6f224e8d6846e58d112107
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-06-06 10:30:32 +0530

    added new font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,11 @@
-import { Inter, Prompt } from 'next/font/google';
+import { Inter, Prompt,Handlee} from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
 export const prompt = Prompt({
     weight: ['700','700'],
+    subsets: ['latin'],
+  });
+  export const handlee = Handlee({
+    weight: ['400','400'],
     subsets: ['latin'],
   });
   // export const tenorSans = Tenor_Sans({
